4ded8f50dd1ea13ca8c0fd1ca439887e146778f8
Luigi <luigi.forte@accenture.com>
2026-04-13 18:12:05 +0200

    header implementation

--- src/_components/Header.tsx
+++ src/_components/Header.tsx
@@ -1,12 +1,36 @@
 import NextLink from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <NextLink href="#">Home</NextLink>
-        <NextLink href="#">About</NextLink>
-        <NextLink href="#">Contact</NextLink>
+    <header className="py-2 mb-2 sticky w-f">
+      <nav className="flex items-center justify-start">
+        <div className="logo-wrapper mr-8">
+          <NextLink href="/" className="logo">
+            <Image
+              src="./lf_logo.svg"
+              width={80}
+              height={80}
+              alt="logo"
+              className="border rounded-full p-1"
+            />
+          </NextLink>
+        </div>
+        <div className="header-right-wrapper flex items-center gap-8">
+          <NextLink href="#" className="flex items-center gap-1">
+            Riguardo me
+          </NextLink>
+          <NextLink href="#" className="flex items-center gap-1">
+            Progetti
+          </NextLink>
+
+          <NextLink href="#" className="flex items-center gap-1">
+            Skill
+          </NextLink>
+          <NextLink href="#" className="flex items-center gap-1">
+            Esperienza
+          </NextLink>
+        </div>
       </nav>
     </header>
   );
