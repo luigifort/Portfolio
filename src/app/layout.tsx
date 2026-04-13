dc9c7f7d7bf7e9052a77c39b86f2c08e612c3300
Luigi <luigi.forte@accenture.com>
2026-04-13 16:15:20 +0200

    add custom layout layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       lang="it"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col layout">
         <Header />
         <main>{children}</main>
       </body>
