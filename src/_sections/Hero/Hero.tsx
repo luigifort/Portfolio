5873731d24e5aa0a93731a8b3c946ebf033ac7ad
Luigi <luigi.forte@accenture.com>
2026-04-13 23:31:23 +0200

    hero section implementation

--- src/_sections/Hero/Hero.tsx
+++ src/_sections/Hero/Hero.tsx
@@ -1,3 +1,33 @@
+import NextLink from "next/link";
+
 export default function Hero() {
-  return <section>Hero</section>;
+  return (
+    <section className=" mx-auto px-6 py-24 ">
+      <div className="text-wrapper">
+        <h1 className="text-3xl md:text-5xl font-semibold leading-tight">
+          Ciao mondo,
+        </h1>
+        <h2 className="text-2xl md:text-3xl font-semibold leading-tight">
+          Benvenuti nel mio portfolio!
+        </h2>
+
+        <p className="mt-6 text-gray-600 max-w-xl">
+          Sono Luigi Forte, sviluppatore front-end
+        </p>
+
+        <div className="mt-8 flex gap-4">
+          <NextLink
+            href="./CV_Luigi_Forte.pdf"
+            className="btn-secondary"
+            target="_blank"
+          >
+            Vedi il CV
+          </NextLink>
+          <NextLink href="#" className="btn-primary">
+            Dai un&apos;occhiata ai progetti
+          </NextLink>
+        </div>
+      </div>
+    </section>
+  );
 }
