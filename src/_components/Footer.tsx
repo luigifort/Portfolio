3cb37f36592b38e75f2367d86fd21e3faceabdde
Luigi <luigi.forte@accenture.com>
2026-04-14 16:46:25 +0200

    add logo to the footer

--- src/_components/Footer.tsx
+++ src/_components/Footer.tsx
@@ -1,4 +1,5 @@
 import NextLink from "next/link";
+import Image from "next/image";
 import { FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
@@ -8,9 +9,20 @@ export default function Footer() {
     <footer className="py-8 gap-8">
       <div className="layout">
         <div className="footer-top flex justify-between items-center">
-          <div>
-            <p className="font-bold">Luigi Forte</p>
-            <p>Frontend Developer</p>
+          <div className="flex">
+            <NextLink href="/" className="logo">
+              <Image
+                src="./lf_logo_light.svg"
+                width={60}
+                height={60}
+                alt="logo"
+                className="border rounded-full object-cover relative bottom-1"
+              />
+            </NextLink>
+            <div>
+              <p className="font-bold">Luigi Forte</p>
+              <p>Frontend Developer</p>
+            </div>
           </div>
 
           <div>
