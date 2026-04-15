6569d36edb8372e6d6c048c02b9d98f4b3fd10b0
Luigi <luigi.forte@accenture.com>
2026-04-15 16:28:26 +0200

    style primary button

--- src/_components/Button.tsx
+++ src/_components/Button.tsx
@@ -13,10 +13,11 @@ export interface ButtonProps extends RACButtonProps {
 
 const button = tv({
   extend: focusRing,
-  base: "relative inline-flex items-center justify-center gap-2 border border-transparent dark:border-white/10 h-9 box-border px-3.5 py-0 [&:has(>svg:only-child)]:px-0 [&:has(>svg:only-child)]:h-8 [&:has(>svg:only-child)]:w-8 font-sans text-sm text-center transition rounded-lg cursor-default [-webkit-tap-highlight-color:transparent]",
+  base: "relative inline-flex items-center justify-center gap-2 border border-transparent dark:border-white/10 h-9 box-border px-3.5 py-0 [&:has(>svg:only-child)]:px-0 [&:has(>svg:only-child)]:h-8 [&:has(>svg:only-child)]:w-8 font-sans text-sm text-center transition rounded-xl cursor-default [-webkit-tap-highlight-color:transparent]",
   variants: {
     variant: {
-      primary: "bg-blue-600 hover:bg-blue-700 pressed:bg-blue-800 text-white",
+      primary:
+        "bg-primary py-3 px-8 opacity-90 hover:opacity-100 hover:cursor-pointer hover:pointer-cursor pressed:bg-primary-dark text-bg",
       secondary:
         "border-black/10 bg-neutral-50 hover:bg-neutral-100 pressed:bg-neutral-200 text-neutral-800 dark:bg-neutral-700 dark:hover:bg-neutral-600 dark:pressed:bg-neutral-500 dark:text-neutral-100",
       destructive: "bg-red-700 hover:bg-red-800 pressed:bg-red-900 text-white",
